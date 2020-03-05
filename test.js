bb5f9589a469d6bf5166552c1b325a526e6d6023
lakshitasetia <35867075+lakshitasetia@users.noreply.github.com>
2020-03-05 22:41:45 +0530

    test.js

---
+++ test.js
@@ -0,0 +1,46 @@
+var a1= document.getElementsByName('q1');
+var a2= document.getElementsByName('q2');
+var a3= document.getElementsByName('q3');
+var a4= document.getElementsByName('q4');
+var a5= document.getElementsByName('q5');
+var a6= document.getElementsByName('q6');
+var a7= document.getElementsByName('q7');
+var a8= document.getElementsByName('q8');
+var a9= document.getElementsByName('q9');
+var a10= document.getElementsByName('q10');
+var a11= document.getElementsByName('q11');
+var a12= document.getElementsByName('q12');
+var a13= document.getElementsByName('q13');
+var a14= document.getElementsByName('q14');
+var a15= document.getElementsByName('q15');
+var a16= document.getElementsByName('q16');
+var a17= document.getElementsByName('q17');
+var a18= document.getElementsByName('q18');
+var a19= document.getElementsByName('q19');
+var a20= document.getElementsByName('q20');
+var a21= document.getElementsByName('q21');
+
+function store() {
+    localStorage.setItem('a1', a1.value);
+    localStorage.setItem('a2', a2.value);
+    localStorage.setItem('a3', a3.value);
+    localStorage.setItem('a4', a4.value);
+    localStorage.setItem('a5', a5.value);
+    localStorage.setItem('a6', a6.value);
+    localStorage.setItem('a7', a7.value);
+    localStorage.setItem('a8', a8.value);
+    localStorage.setItem('a9', a9.value);
+    localStorage.setItem('a10', a10.value);
+    localStorage.setItem('a11', a11.value);
+    localStorage.setItem('a12', a12.value);
+    localStorage.setItem('a13', a13.value);
+    localStorage.setItem('a14', a14.value);
+    localStorage.setItem('a15', a15.value);
+    localStorage.setItem('a16', a16.value);
+    localStorage.setItem('a17', a17.value);
+    localStorage.setItem('a18', a18.value);
+    localStorage.setItem('a19', a19.value);
+    localStorage.setItem('a20', a20.value);
+    localStorage.setItem('a21', a21.value);
+}
+
