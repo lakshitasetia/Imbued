42d74850ca2f1bde6c412f2c1dd1f7401026b231
lakshitasetia <35867075+lakshitasetia@users.noreply.github.com>
2020-03-05 16:45:54 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 var fname = document.getElementById('fname');
 var lname = document.getElementById('lname');
-var emailid= document.getElementById('email');
+var email= document.getElementById('email');
 var pw= document.getElementById('pw');
 var number= document.getElementById('number');
 var age= document.getElementById('age');
@@ -9,7 +9,7 @@ var age= document.getElementById('age');
 function store() {
     localStorage.setItem('fname', fname.value);
     localStorage.setItem('lname', lname.value);
-    localStorage.setItem('emailid', email.value);
+    localStorage.setItem('email', email.value);
     localStorage.setItem('pw', pw.value);
     localStorage.setItem('number', number.value);
     localStorage.setItem('age', age.value);
@@ -19,7 +19,7 @@ function store() {
 function checkin() {
 
     // stored data from the register-form
-    var storedEmail = localStorage.getItem('emailid');
+    var storedEmail = localStorage.getItem('email');
 
     var storedPw = localStorage.getItem('pw');
 
